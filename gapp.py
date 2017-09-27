21f45bcddc12a283b9742e6038ddea610778c827
root <root@localhost.localdomain>
2017-09-27 09:41:15 +0530

    Initial commit

---
+++ gapp.py
@@ -0,0 +1,10 @@
+print "hi say yout monthly expense "
+expense =  int(raw_input('>>'))
+print "hi say yout monthly emi "
+emi =  int(raw_input('>>'))
+print "hi say yout monthly loan "
+loan =  int(raw_input('>>'))
+print "hi say yout monthly total gap expected "
+total_gap =  int(raw_input('>>'))
+highgap = expense + emi + loan + total_gap
+print "gap is = ", highgap
